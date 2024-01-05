b199dfd85464e2b1cd520b67611b9dd6571f9109
Lenu-com <oretyan@icloud.com>
2024-01-06 02:44:26 +0900

    無駄なリソースの使用箇所を修正

--- DiscordBotConnector.py
+++ DiscordBotConnector.py
@@ -1,16 +1,14 @@
 import os
 import discord
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 async def discord_bot_run():
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
     @client.event
     async def on_message(message):
         if message.author.bot:
             return
-        await message.channel.send(message.content)
 
     await client.start(os.environ["DISCORD_API_KEY"])
